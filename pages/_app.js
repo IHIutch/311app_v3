a04d26c96ef1db95f2a609586fb9011313b723b5
IHIutch <jbhutch01@gmail.com>
2021-04-04 11:06:27 -0400

    Set up app base

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
-export default MyApp
+export default MyApp;
