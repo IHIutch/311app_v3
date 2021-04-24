3c5962cf985ad4c5d328b61ab7fcf76461248790
IHIutch <jbhutch01@gmail.com>
2021-04-24 13:11:44 -0400

    Organize upload functions

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,10 +1,6 @@
-import { supabase } from '@/utils/supabase'
 import { resStatusType } from '@/utils/types'
-import axios from 'redaxios'
-import { v4 as uuidv4 } from 'uuid'
-import FormData from 'form-data'
-import formidable from 'formidable'
-import fs from 'fs'
+
+import { handleImageReq, uploadFile } from '@/utils/api/uploads'
 
 export default async function handler(req, res) {
   const { method } = req
@@ -12,38 +8,10 @@ export default async function handler(req, res) {
   switch (method) {
     case 'POST':
       try {
-        const getFileName = async () => {
-          const form = new formidable.IncomingForm()
-          form.uploadDir = './'
-          form.keepExtensions = true
-          return await new Promise((resolve, reject) => {
-            form.parse(req, (err, fields, files) => {
-              if (err) reject(err)
-              resolve({
-                name: files.file.name,
-                path: files.file.path,
-                file: fs.readFileSync(files.file.path),
-              })
-            })
-          })
-        }
-        const { name, path, file } = await getFileName()
-        fs.unlinkSync(path)
+        const { name, file } = await handleImageReq(req)
+        const filePath = await uploadFile(name, file)
 
-        const fileExt = name.split('.').pop()
-        const fileName = `public/${uuidv4()}.${fileExt}`
-        const bucketName = 'buffalo311'
-        const url = `${supabase.storage.url}/object/${bucketName}/${fileName}`
-        const headers = supabase.storage.headers
-
-        const formData = new FormData()
-        formData.append('file', file, fileName)
-
-        const { error } = await axios.post(url, formData, {
-          headers,
-        })
-        if (error) throw new Error(error)
-        res.status(resStatusType.SUCCESS).json(fileName)
+        res.status(resStatusType.SUCCESS).json(filePath)
       } catch (error) {
         console.error(error)
         res.status(resStatusType.BAD_REQUEST).json(error)
---
+++ utils/api/uploads.js
@@ -0,0 +1,44 @@
+import { supabase } from '@/utils/supabase'
+import axios from 'redaxios'
+import { v4 as uuidv4 } from 'uuid'
+import FormData from 'form-data'
+import formidable from 'formidable'
+import fs from 'fs'
+
+export const uploadFile = async (name, file) => {
+  const fileExt = name.split('.').pop()
+  const filePath = `public/${uuidv4()}.${fileExt}`
+  const bucketName = 'buffalo311'
+  const url = `${supabase.storage.url}/object/${bucketName}/${filePath}`
+  const headers = supabase.storage.headers
+
+  const formData = new FormData()
+  formData.append('file', file, filePath)
+
+  const { error } = await axios.post(url, formData, {
+    headers,
+  })
+  if (error) throw new Error(error)
+  return filePath
+}
+
+export const handleImageReq = async (req) => {
+  const getFileName = async () => {
+    const form = new formidable.IncomingForm()
+    form.uploadDir = './'
+    form.keepExtensions = true
+    return await new Promise((resolve, reject) => {
+      form.parse(req, (err, fields, files) => {
+        if (err) reject(err)
+        resolve({
+          name: files.file.name,
+          path: files.file.path,
+          file: fs.readFileSync(files.file.path),
+        })
+      })
+    })
+  }
+  const { name, path, file } = await getFileName()
+  fs.unlinkSync(path)
+  return { name, file }
+}
