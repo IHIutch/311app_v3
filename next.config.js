f6a731d72d7e5ed71855fc4f30c1c0a75644ba6b
IHIutch <jbhutch01@gmail.com>
2021-05-11 14:33:35 -0400

    Install sentry

--- next.config.js
+++ next.config.js
@@ -1,12 +1,33 @@
-module.exports = {
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = {
+  // Your existing module.exports
   env: {
     BASE_URL:
       process.env.NODE_ENV === 'production'
-        ? // ? process.env.NEXT_PUBLIC_VERCEL_URL
-          'https://311app.vercel.app'
+        ? process.env.NEXT_PUBLIC_VERCEL_URL
         : 'http://localhost:3000',
   },
   future: {
     webpack5: true,
   },
 }
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions)
