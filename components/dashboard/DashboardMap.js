152e0dcb080ad281a9206056a4cd244b9a85f9b3
IHIutch <jbhutch01@gmail.com>
2021-06-12 13:46:04 -0400

    Update map and mapstyles

--- components/dashboard/DashboardMap.js
+++ components/dashboard/DashboardMap.js
@@ -1,49 +1,160 @@
-import Head from 'next/head'
-import React from 'react'
-import { CircleMarker, MapContainer, TileLayer } from 'react-leaflet'
+import React, { useState } from 'react'
+import {
+  Box,
+  Text,
+  useToken,
+  Link,
+  chakra,
+  Flex,
+  Icon,
+  Tag,
+  HStack,
+} from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  CircleMarker,
+  FeatureGroup,
+  MapContainer,
+  Popup,
+  TileLayer,
+  useMap,
+  ZoomControl,
+} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import { reportStatusType } from '@/utils/types'
+import {
+  UilLockOpenAlt,
+  UilMap,
+  UilListUl,
+  UilSlidersV,
+} from '@iconscout/react-unicons'
+import { formatDate } from '@/utils/functions'
 
 export default function DashboardMap({ markers }) {
+  const [popup, setPopup] = useState(null)
+  const handleMarkerClick = (data) => {
+    // console.log(data)
+    setPopup(data)
+  }
+
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-          key="leafletCss"
-        />
-      </Head>
+    <Box
+      boxSize="100%"
+      sx={{
+        '.leaflet-popup-content-wrapper': {
+          rounded: 'md',
+          shadow: 'lg',
+        },
+        '.leaflet-popup-content': {
+          m: '0',
+          p: '4',
+        },
+      }}
+    >
       <MapContainer
         style={{ height: '100%', width: '100%' }}
         center={[42.886, -78.879]}
         zoom={13}
         preferCanvas={true}
+        zoomControl={false}
       >
+        <ZoomControl position="topright" />
         <TileLayer
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`}
         />
-        {markers &&
-          markers.map((m, idx) => (
-            <CircleMarker
-              key={idx}
-              center={{ lat: m.lat, lng: m.lng }}
-              radius={6}
-              eventHandlers={{
-                click: () => {
-                  console.log(m)
-                },
-              }}
-              pathOptions={{
-                color: 'blue',
-                fillOpacity: 1,
-                opacity: 0.2,
-                weight: 10,
-              }}
-            />
-          ))}
+        <FeatureGroup>
+          {markers &&
+            markers.map((m, idx) => (
+              <MapMarker
+                key={idx}
+                markerColor={m.reportType.markerColor}
+                center={{ lat: m.lat, lng: m.lng }}
+                radius={6}
+                markerClickHandler={() => handleMarkerClick(m)}
+              />
+            ))}
+
+          <Popup>
+            {popup && (
+              <Box>
+                <Box mb="3">
+                  {popup.status === reportStatusType.CREATED && (
+                    <Flex color="green" align="center">
+                      <Icon boxSize="5" as={UilLockOpenAlt} />
+                      <Text as="span" fontSize="sm" fontWeight="medium" ml="1">
+                        Open
+                      </Text>
+                    </Flex>
+                  )}
+                </Box>
+                <Box mb="4">
+                  <Text
+                    sx={{
+                      '&&': {
+                        fontWeight: 'medium',
+                        fontSize: 'md',
+                        m: '0',
+                        mb: '1',
+                        color: 'black',
+                      },
+                    }}
+                  >
+                    {popup.reportType.group} - {popup.reportType.name}
+                  </Text>
+                  <Text
+                    as="span"
+                    fontSize="sm"
+                    color="gray.600"
+                    // fontWeight="medium"
+                  >
+                    #{popup.id} • Opened on {formatDate(popup.createdAt)}
+                  </Text>
+                </Box>
+                <Box textAlign="right">
+                  <NextLink passHref href={`reports/${popup.id}`}>
+                    <Link
+                      sx={{
+                        '&&': {
+                          color: 'blue.500',
+                          fontSize: 'md',
+                          fontWeight: 'medium',
+                        },
+                      }}
+                    >
+                      View Report
+                    </Link>
+                  </NextLink>
+                </Box>
+              </Box>
+            )}
+          </Popup>
+        </FeatureGroup>
       </MapContainer>
-    </>
+    </Box>
+  )
+}
+
+const MapMarker = ({ markerColor, markerClickHandler, ...props }) => {
+  const map = useMap()
+  const handleClick = (latlng) => {
+    map.flyTo(latlng, map.getZoom())
+    markerClickHandler()
+  }
+
+  const [color] = useToken('colors', [markerColor])
+  return (
+    <CircleMarker
+      {...props}
+      eventHandlers={{
+        click: (e) => handleClick(e.latlng),
+      }}
+      pathOptions={{
+        color,
+        fillOpacity: 1,
+        opacity: 0.3,
+        weight: 10,
+      }}
+    />
   )
 }
--- components/reportCreation/MapboxEmbed.js
+++ components/reportCreation/MapboxEmbed.js
@@ -1,5 +1,4 @@
 import { Text } from '@chakra-ui/react'
-import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import {
   MapContainer,
@@ -8,21 +7,13 @@ import {
   useMapEvents,
   Tooltip,
 } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default function MapboxEmbed({ latLng, handleSetLocation }) {
   const [center] = useState(latLng || [42.886, -78.879])
 
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-          key="leafletCss"
-        />
-      </Head>
       <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
