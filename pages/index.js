21f699ac36a63d70f61c0b164167cb31ea4ff11c
IHIutch <jbhutch01@gmail.com>
2021-06-13 10:38:34 -0400

    Fix SSR hydration issue

--- pages/index.js
+++ pages/index.js
@@ -122,93 +122,95 @@ export default function Home() {
         <Box position="fixed" top="0" pt="28" w="100%" h="100%">
           <Container fluid h="100%" px="0">
             <Grid h="100%" w="100%" templateColumns={{ lg: 'repeat(2, 1fr)' }}>
-              {(isLargerThanLg || isShowingMapMobile) && (
-                <GridItem boxSize="100%" borderRightWidth="1px">
-                  {reports && <DashboardMap markers={Object.values(reports)} />}
-                </GridItem>
-              )}
-              {(isLargerThanLg || !isShowingMapMobile) && (
-                <GridItem h="100%" overflow="auto" p="6">
-                  <Box>
-                    {reports && !isReportsLoading ? (
-                      <Stack dir="column" spacing="0">
-                        {Object.values(reports).map((r, idx) => (
-                          <LinkBox
-                            key={idx}
-                            p="3"
-                            bg="white"
-                            borderWidth="1px"
-                            borderTopWidth={idx !== 0 && '0'}
-                            borderTopRadius={idx === 0 && 'md'}
-                            borderBottomRadius={
-                              idx === Object.keys(reports).length - 1 && 'md'
-                            }
-                            transition="all ease 0.2s"
-                            _hover={{ bg: 'gray.100' }}
-                          >
-                            <Flex>
-                              <Box>
-                                {r.status === reportStatusType.CREATED && (
-                                  <Icon
-                                    color="green"
-                                    boxSize="6"
-                                    as={UilLockOpenAlt}
-                                  />
-                                )}
-                              </Box>
-                              <Box ml="2">
-                                <NextLink passHref href={`reports/${r.id}`}>
-                                  <LinkOverlay fontWeight="semibold">
-                                    <Text lineHeight="1.4">
-                                      {r.reportType.group} - {r.reportType.name}
-                                    </Text>
-                                  </LinkOverlay>
-                                </NextLink>
-                                <Box>
-                                  <Text
-                                    as="span"
-                                    fontSize="sm"
-                                    color="gray.600"
-                                  >
-                                    #{r.id} • Opened on{' '}
-                                    {formatDate(r.createdAt)}
+              <GridItem
+                boxSize="100%"
+                borderRightWidth="1px"
+                // d={{ base: isShowingMapMobile ? 'block' : 'none', lg: 'block' }}
+              >
+                {reports && (isLargerThanLg || isShowingMapMobile) && (
+                  <DashboardMap markers={Object.values(reports)} />
+                )}
+              </GridItem>
+              <GridItem
+                h="100%"
+                overflow="auto"
+                p="6"
+                d={{ base: isShowingMapMobile ? 'none' : 'block', lg: 'block' }}
+              >
+                <Box>
+                  {reports && !isReportsLoading ? (
+                    <Stack dir="column" spacing="0">
+                      {Object.values(reports).map((r, idx) => (
+                        <LinkBox
+                          key={idx}
+                          p="3"
+                          bg="white"
+                          borderWidth="1px"
+                          borderTopWidth={idx !== 0 && '0'}
+                          borderTopRadius={idx === 0 && 'md'}
+                          borderBottomRadius={
+                            idx === Object.keys(reports).length - 1 && 'md'
+                          }
+                          transition="all ease 0.2s"
+                          _hover={{ bg: 'gray.100' }}
+                        >
+                          <Flex>
+                            <Box>
+                              {r.status === reportStatusType.CREATED && (
+                                <Icon
+                                  color="green"
+                                  boxSize="6"
+                                  as={UilLockOpenAlt}
+                                />
+                              )}
+                            </Box>
+                            <Box ml="2">
+                              <NextLink passHref href={`reports/${r.id}`}>
+                                <LinkOverlay fontWeight="semibold">
+                                  <Text lineHeight="1.4">
+                                    {r.reportType.group} - {r.reportType.name}
                                   </Text>
-                                </Box>
+                                </LinkOverlay>
+                              </NextLink>
+                              <Box>
+                                <Text as="span" fontSize="sm" color="gray.600">
+                                  #{r.id} • Opened on {formatDate(r.createdAt)}
+                                </Text>
                               </Box>
-                            </Flex>
-                          </LinkBox>
-                        ))}
-                      </Stack>
-                    ) : (
-                      <Stack dir="column" spacing="0">
-                        {[...Array(12)].map((r, idx) => (
-                          <Box
-                            key={idx}
-                            p="3"
-                            bg="white"
-                            borderWidth="1px"
-                            _notFirst={{ borderTopWidth: '0' }}
-                            _first={{
-                              borderTopRadius: 'md',
-                            }}
-                            _last={{
-                              borderBottomRadius: 'md',
-                            }}
-                          >
-                            <Flex>
-                              <SkeletonCircle size="6" />
-                              <Box flexGrow="1" ml="2">
-                                <Skeleton height="5" mb="3" w="96" />
-                                <Skeleton height="3" w="48" />
-                              </Box>
-                            </Flex>
-                          </Box>
-                        ))}
-                      </Stack>
-                    )}
-                  </Box>
-                </GridItem>
-              )}
+                            </Box>
+                          </Flex>
+                        </LinkBox>
+                      ))}
+                    </Stack>
+                  ) : (
+                    <Stack dir="column" spacing="0">
+                      {[...Array(12)].map((r, idx) => (
+                        <Box
+                          key={idx}
+                          p="3"
+                          bg="white"
+                          borderWidth="1px"
+                          _notFirst={{ borderTopWidth: '0' }}
+                          _first={{
+                            borderTopRadius: 'md',
+                          }}
+                          _last={{
+                            borderBottomRadius: 'md',
+                          }}
+                        >
+                          <Flex>
+                            <SkeletonCircle size="6" />
+                            <Box flexGrow="1" ml="2">
+                              <Skeleton height="5" mb="3" w="96" />
+                              <Skeleton height="3" w="48" />
+                            </Box>
+                          </Flex>
+                        </Box>
+                      ))}
+                    </Stack>
+                  )}
+                </Box>
+              </GridItem>
             </Grid>
           </Container>
         </Box>
